b3888cd9e58f7f0cec9497847a4298a776efa279
Luis <luistroche@gmail.com>
2020-09-28 21:57:22 -0400

    added exercise 8

--- regex-exercises-1.js
+++ regex-exercises-1.js
@@ -70,3 +70,17 @@ let regex7 = /.un/gi;
 let result7 = regex7.test(temon);
 
 console.log(`${result7}  <-- result7`);
+
+// #8
+// match character with multiple possibilities
+// Use a character class with vowels (a, e, i, o, u) in your regex vowelRegex to find all the vowels in the string quoteSample.
+
+// Note
+// Be sure to match both upper- and lowercase vowels.
+
+let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it."
+let vowelRegex = /[aeiou]/gi;
+let result8 = quoteSample.match(vowelRegex);
+
+console.log(result8);
+
